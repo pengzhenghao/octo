0c13739d5086190ba07fa33688621acf87e67d34
pengzhenghao <pzh@cs.ucla.edu>
2024-04-01 18:19:50 -0700

    run

--- 0401.sh
+++ 0401.sh
@@ -1,5 +1,4 @@
 nohup python examples/02_finetune_new_observation_action_METADRIVE.py \
 --config=examples/configs/finetune_metadrive_config.py:full,language_conditioned \
---config.batch_size=32 \
 --name=0401_Full_NoLidar_Lang_NoNavi \
 > 0401_NoNavi.log 2>&1 &
